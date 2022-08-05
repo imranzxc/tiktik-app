7bec52add50d8f7eeb49a4010b352458897b7954
Imran <imran.bursagov@gmail.com>
2022-08-05 21:36:54 +0300

    final

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'raccoon-city.ru'
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
+
---
+++ utils/client.ts
@@ -0,0 +1,10 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+  projectId: 'rteemb8j',
+  dataset: 'production',
+  apiVersion: '2022-03-10',
+  useCdn: false,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
+
